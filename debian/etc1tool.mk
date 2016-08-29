72a1fe8b8ee3df7ddff4492ce54c62f0b31c3b63
Kai-Chung Yan (殷啟聰) <seamlikok@gmail.com>
2016-08-29 16:52:35 +0800

    Search for Android libraries in multiarch path

--- debian/etc1tool.mk
+++ debian/etc1tool.mk
@@ -2,7 +2,8 @@ NAME = etc1tool
 SOURCES = etc1tool.cpp
 SOURCES := $(foreach source, $(SOURCES), tools/etc1tool/$(source))
 CPPFLAGS += -Iinclude -I/usr/include/android
-LDFLAGS += -Wl,-rpath=/usr/lib/android -lpng -L/usr/lib/android -lETC1
+LDFLAGS += -Wl,-rpath=/usr/lib/$(DEB_HOST_MULTIARCH)/android \
+           -lpng -L/usr/lib/$(DEB_HOST_MULTIARCH)/android -lETC1
 
 $(NAME): $(SOURCES)
 	$(CXX) $^ -o $@ $(CFLAGS) $(CPPFLAGS) $(LDFLAGS)
