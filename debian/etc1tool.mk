bff64e793bb3b763526f7592266f6c29e8efb845
Kai-Chung Yan (殷啟聰) <seamlikok@gmail.com>
2016-10-01 15:36:24 +0800

    Refresh build scripts

--- debian/etc1tool.mk
+++ debian/etc1tool.mk
@@ -1,12 +1,12 @@
 NAME = etc1tool
 SOURCES = etc1tool.cpp
 SOURCES := $(foreach source, $(SOURCES), tools/etc1tool/$(source))
-CPPFLAGS += -Iinclude -I/usr/include/android
+CPPFLAGS += -Iinclude
 LDFLAGS += -Wl,-rpath=/usr/lib/$(DEB_HOST_MULTIARCH)/android \
            -lpng -L/usr/lib/$(DEB_HOST_MULTIARCH)/android -lETC1
 
 $(NAME): $(SOURCES)
-	$(CXX) $^ -o $@ $(CFLAGS) $(CPPFLAGS) $(LDFLAGS)
+	$(CXX) $^ -o $@ $(CXXFLAGS) $(CPPFLAGS) $(LDFLAGS)
 
 clean:
 	$(RM) $(NAME)
